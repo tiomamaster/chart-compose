c15bf530ae4f64b8e08d3d8fadef16e8c8090a08
tioma-master <tioma.master@gmail.com>
2022-05-06 17:48:24 +0300

    Compose: improve chart bounds calculation

--- chart-compose/src/main/java/com/gmail/tiomamaster/chart/compose/Chart.kt
+++ chart-compose/src/main/java/com/gmail/tiomamaster/chart/compose/Chart.kt
@@ -14,9 +14,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.DrawScope
-import androidx.compose.ui.graphics.drawscope.clipRect
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.drawscope.withTransform
 import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -24,11 +23,12 @@ import androidx.compose.ui.unit.dp
 private val animSpecX = tween<Float>(100, easing = LinearEasing)
 private val animSpecY = tween<Float>(150, easing = LinearEasing)
 
+private const val chartStrokeWidth = 5f
 private val chartPaint = Paint().apply {
     isAntiAlias = true
     isDither = true
     style = Paint.Style.STROKE
-    strokeWidth = 5f
+    strokeWidth = chartStrokeWidth
 }
 
 private var labelsPaint: Paint? = null
@@ -59,7 +59,7 @@ internal fun Chart(
     var canvasSize by remember { mutableStateOf(Size.Zero) }
     val xLabelsHeight = labelSettings?.run { (labelsSize + xLabelsTopPadding).toPx() } ?: 0f
     val chartHeight = remember(labelSettings, canvasSize) {
-        canvasSize.height - xLabelsHeight
+        canvasSize.height - xLabelsHeight - chartStrokeWidth
     }
     val sourcePaths = remember(data, canvasSize, chartHeight) {
         if (canvasSize == Size.Zero) null
@@ -95,13 +95,30 @@ internal fun Chart(
             }
         }
     }
+    val lineTopMargin = 32.dp.toPx()
+    val linesCount = 5
+    val lineYCoords = remember(chartHeight, labelSettings) {
+        if (labelSettings == null) return@remember null
+        List(linesCount + 1) {
+            chartHeight - it * (chartHeight - lineTopMargin) / linesCount
+        }
+    }
     Canvas(modifier) {
         if (canvasSize == Size.Zero) {
             canvasSize = size
         } else {
-            val yLineCoords = labelSettings?.let { drawLines(chartHeight, it) }
+            lineYCoords?.forEach {
+                drawLine(
+                    Color.LightGray,
+                    Offset(labelSettings!!.yLabelsStartPadding.toPx(), it + chartStrokeWidth),
+                    Offset(size.width, it + chartStrokeWidth)
+                )
+            }
 
-            clipRect(bottom = chartHeight) {
+            withTransform({
+                clipRect(bottom = chartHeight + chartStrokeWidth)
+                inset(0f, chartStrokeWidth / 2, 0f, chartStrokeWidth / 2)
+            }) {
                 drawIntoCanvas {
                     paths?.forEachIndexed { i, path ->
                         it.nativeCanvas.drawPath(
@@ -111,9 +128,26 @@ internal fun Chart(
                 }
             }
 
-            yLineCoords?.also {
-                drawYLabels(it, data, labelSettings)
-                drawXLabels(data, labelSettings.xLabelsFormatter)
+            if (lineYCoords == null) return@Canvas
+            drawIntoCanvas {
+                with(it.nativeCanvas) {
+                    lineYCoords.forEach { y ->
+                        drawText(
+                            data.getYValueByCoord(y).toString(),
+                            labelSettings!!.yLabelsStartPadding.toPx(),
+                            y,
+                            getLabelsPaint(labelSettings.labelsSize.toPx())
+                        )
+                    }
+
+                    data.getXLabels(
+                        labelsPaint!!,
+                        size.width,
+                        labelSettings!!.xLabelsFormatter
+                    ).forEach { (coord, text) ->
+                        drawText(text, coord, size.height, labelsPaint!!)
+                    }
+                }
             }
         }
     }
@@ -125,41 +159,3 @@ internal class LabelSettings(
     val xLabelsTopPadding: Dp,
     val xLabelsFormatter: (xValue: Number) -> String
 )
-
-private fun DrawScope.drawLines(chartHeight: Float, settings: LabelSettings): List<Float> {
-    val linesCount = 5
-    val k = (chartHeight - 32.dp.toPx()) / linesCount
-    return List(linesCount + 1) {
-        val y = chartHeight - it * k
-        drawLine(
-            Color.LightGray,
-            Offset(settings.yLabelsStartPadding.toPx(), y),
-            Offset(size.width, y)
-        )
-        y
-    }
-}
-
-private fun DrawScope.drawYLabels(
-    yLineCoords: List<Float>,
-    data: ChartData<Number, Number>,
-    labelSettings: LabelSettings
-) = yLineCoords.forEach { y ->
-    drawIntoCanvas {
-        it.nativeCanvas.drawText(
-            data.getYValueByCoord(y).toString(),
-            labelSettings.yLabelsStartPadding.toPx(),
-            y - 4.dp.toPx(),
-            getLabelsPaint(labelSettings.labelsSize.toPx())
-        )
-    }
-}
-
-private fun DrawScope.drawXLabels(
-    data: ChartData<Number, Number>,
-    formatter: ((xValue: Number) -> String)
-) = data.getXLabels(labelsPaint!!, size.width, formatter).forEach { (coord, text) ->
-    drawIntoCanvas {
-        it.nativeCanvas.drawText(text, coord, size.height, labelsPaint!!)
-    }
-}
